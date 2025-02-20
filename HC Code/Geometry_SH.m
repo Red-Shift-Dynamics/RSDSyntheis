6688fb372e28cefe1890f08ee76e51d94821bd88
Enrique-A-P <enrique.palacio.42@gmail.com>
2025-02-20 14:40:43 -0600

    2/20 2:40
    Enrique

--- HC Code/Geometry_SH.m
+++ HC Code/Geometry_SH.m
@@ -1,12 +1,13 @@
 %% Wetted Area to Planform Area Ratio Calculator - Superheavy
 % [SH.Kw] = Geometry(SH.tau, SH.Spln, SS.Swet)
-function [SH] = Geometry(SH, SS)
+function [SH] = Geometry_SH(SH, SS)
     
     % [m^2] Wetted Area - Starship
-    SH.Swet = sqrt(SH.tau) * 10000;
+    %SH.Swet = sqrt(SH.tau) * 10000;
 
     % Wetted to Planform Area Ratio - Starship
-    SH.Kw = (SH.Swet + SS.Swet)/SH.Spln;
+    %SH.Kw = (SH.Swet + SS.Swet)/SH.Spln;
+    SH.Kw = -56.368 * (SH.tau^2) + 23.374 * SH.tau + 1.0887;
 
 end
 
