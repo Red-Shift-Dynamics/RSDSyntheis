0aa804aae9e95c2c4bbbd5f87d6ebde05047d1eb
Enrique-A-P <enrique.palacio.42@gmail.com>
2025-02-20 14:49:43 -0600

    Revert "2/20 2:40"
    This reverts commit 0670d051ec3af8ed07e2d9273bc942c8257d0a8a.

--- HC Code/Trajectory_SS.m
+++ HC Code/Trajectory_SS.m
@@ -2,11 +2,20 @@
 %[SS.WR] = Trajectory(MTV.Vsep, SS.TOGW)
 function [SS] = Trajectory_SS(MTV, SS)
     
-    % [kg] Propellant Weight - Starship
-    SS.Wppl = 1000 * MTV.hleo;
+    % % [kg] Propellant Weight - Starship
+    % SS.Wppl = 1000 * MTV.hleo;
+    
+    % [kg] Starship TOGW;
+    TOGW2 = SS.TOGW;
+
+    Isp = 500;      % [s]
+    tb = 10;        % [s]
+
+    % [m/s] Separation Velocity
+    Vsep = MTV.v_sep;
 
     % Weight Ratio - Starship
-    SS.WR = SS.TOGW/(SS.TOGW - SS.Wppl);
+    SS.WR = WR(~, TOGW2, Isp, tb, Vsep);
     
 end
 
